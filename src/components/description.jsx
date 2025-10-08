cb86ce4d3656dd475b39a8e98bf48ebc7c955cf5
Gingersmurlf <theodorenghoff@gmail.com>
2025-10-08 10:56:43 +0200

    aflever

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -1,4 +1,25 @@
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router";
 
 export default function Description() {
-    
+  return (
+    <div className="wrapper">
+      <header>
+        <div>
+          <Link to="/">
+            <ArrowLeft />
+          </Link>
+          <figure>
+            <img className="pfp" src="./assets/kanye-east.gif" alt="Profile" />
+          </figure>
+        </div>
+        <figure>
+            <img src="burger" alt="burger" />
+        </figure>
+      </header>
+      <main>
+        <h1>burger</h1>
+      </main>
+    </div>
+  );
 }
--- src/components/notfound.jsx
+++ src/components/notfound.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router";
+
 export default function NotFound() {
-    
+    return(
+        <>
+            <Link to="/"><h1>404</h1>
+                <h2>Gå tilbage til forsiden</h2>
+            </Link>
+        </>
+    )
 }
